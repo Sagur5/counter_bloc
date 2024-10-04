aed5c56eebffbcf1db9aa4faa85cef4327650830
Md. Jahid Hossain Sagur <123995838+Sagur5@users.noreply.github.com>
2024-10-04 19:54:36 +0600

    Update block_counter_state.dart

--- lib/bloc/block_counter_state.dart
+++ lib/bloc/block_counter_state.dart
@@ -1,18 +1,4 @@
-// // ignore_for_file: use_super_parameters
 
-// abstract class BlockCounterState {
-//   final int count;
-
-//   BlockCounterState(this.count);
-// }
-
-// class CounterInitial extends BlockCounterState {
-//   CounterInitial() : super(0);
-// }
-
-// class CounterUpdated extends BlockCounterState {
-//   CounterUpdated(int count) : super(count);
-// }
 abstract class BlockCounterState {
   final int count;
 
